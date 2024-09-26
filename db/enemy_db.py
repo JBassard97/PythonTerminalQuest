9a5b1ef9c788fe3a727d6bface6c8689e39a8cf8
Jbassard <jonathanacciarito@gmail.com>
2024-09-25 22:33:38 -0400

    created 3 more battle_sprites

--- db/enemy_db.py
+++ db/enemy_db.py
@@ -3,19 +3,21 @@ enemy_data = {
         "spider": {"sprite_name": "spider", "display_name": "Unwelcome Spider"},
     },
     "ember_mountain": {
-        "phoenix": {"sprite_name": "phoenix", "display_name": "Re-Alived Firebird"}
+        "phoenix": {"sprite_name": "phoenix", "display_name": "Re-Alived Firebird"},
+        "devil": {"sprite_name": "devil", "devil_name": "Devil-ling"},
     },
     "lushgrove": {
         "flower": {"sprite_name": "flower", "display_name": "Maneating Flower"},
         "gryphon": {"sprite_name": "gryphon", "display_name": "Territorial Gryphon"},
         "gorilla": {"sprite_name": "gorilla", "display_name": "Shredded Gorilla"},
+        "deer": {"sprite_name": "deer", "display_name": "Hormonal Deer"},
     },
     "cinderdune": {
-        "scorpion": {
-            "sprite_name": "scorpion",
-            "display_name": "Venomous Scorpion",
-        },
+        "scorpion": {"sprite_name": "scorpion", "display_name": "Venemous Scorpion"},
         "skeleton": {"sprite_name": "skeleton", "display_name": "Dehydrated Dead"},
     },
-    "riverfall": {},
+    "riverfall": {
+        "beaver": {"sprite_name": "beaver", "display_name": "Rabid Beaver"},
+        "squid": {"sprite_name": "squid", "display_name": "Merciless Squid"},
+    },
 }
